27e8eb81098376d83e2716ea5d6b681139a67b52
n1kno <125561616+n1kno@users.noreply.github.com>
2026-05-17 07:19:45 +0400

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,22 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/n1kno/practic8/internal/handler"
+)
+
+func main() {
+	// Регистрируем маршруты
+	http.HandleFunc("/", handler.FormHandler)
+	http.HandleFunc("/greet", handler.GreetFormHandler)
+	http.HandleFunc("/list", handler.ListHTMLHandler)
+	http.HandleFunc("/api/greet", handler.GreetAPIHandler)
+	http.HandleFunc("/api/list", handler.ListAPIHandler)
+
+	fmt.Println("Сервис запущен на http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
